cc045477e3e49dc1164a1b367c2f2be6018db2cc
Tarcisio Philips <tarcisiogabriel.philips@gmail.com>
2024-06-22 20:46:43 -0300

    feat: Theme and Account Menu

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,17 @@ import { Helmet, HelmetProvider } from "react-helmet-async";
 import { RouterProvider } from "react-router-dom";
 import { Toaster } from "sonner";
 
+import { ThemeProvider } from "./components/theme/theme-provider";
 import { router } from "./routes";
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | ADM" />
-      <Toaster richColors />
-      <RouterProvider router={router} />
+      <ThemeProvider storageKey="adm-theme" defaultTheme="dark">
+        <Helmet titleTemplate="%s | ADM" />
+        <Toaster richColors />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   );
 }
